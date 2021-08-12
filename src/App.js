39f1ea4bc672fcfcebd269bfa7d6d62fbe756825
victorgbaye <victorgbayedev@gmail.com>
2021-08-12 11:16:28 -0700

    first commit for forbes project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import './App.css'
+
+import {IoIosArrowDown} from "react-icons/io";
+import {IoIosArrowUp} from "react-icons/io";
+
+
+const url = 'https://forbes400.herokuapp.com/api/forbes400/getAllBillionaires';
 
 function App() {
+  const [people, setPeople] = useState([]);
+  const [loading, setLoading] = useState(true)
+  const [showMore, setShowMore] = useState(false)
+
+  const fetchPeople = async () => {
+    const response = await fetch(url)
+    const people = await response.json()
+    setPeople(people);
+    setLoading(false)
+    console.log(people)
+  }
+
+  useEffect(()=>{
+    fetchPeople();
+  },[]);
+
+  if(loading){
+    return(
+      <h2>loading</h2>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    <>
+      <table className='forbes__table'>
+
+                <thead>
+                  <tr>
+                    <th>RANK</th>
+                    <th>NAME</th>
+                    <th>NET WORTH</th>
+                    <th>COUNTRY / TERRITORY</th>
+                    <th>SOURCE</th>
+                    <th>INDUSTRY</th>
+                  </tr>
+                </thead>
+                
+
+      {people.map((item)=>{
+        const {person, rank, source,personName,finalWorth, industries, squareImage, countryOfCitizenship, bios} = item
+        return(
+          <>
+            
+                <tbody>
+                  <tr>
+                    <td>{rank}.</td>
+                    <td>{personName}</td>
+                    <td>${finalWorth} B</td>
+                    <td>{countryOfCitizenship}</td>
+                    <td>{source}</td>
+                    <td>{industries}</td>
+                    <td><button onClick={()=>setShowMore(!showMore)}>
+                          btn
+                      </button></td>
+                  </tr>
+                </tbody>
+                {showMore &&
+                <section>
+                  <img src={squareImage} alt="" />
+                  <h3>{bios}</h3>
+                </section>
+                
+                }
+            
+          </>
+        )
+
+      })}
+      </table>
+    </>
+  )
 }
 
-export default App;
+
+export default App
---
+++ src/People.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function People(people) {
+    return (
+        <>
+            {people.map((item)=>{
+                const {person} =item
+                return(
+                    <h2>
+                       {person.name}
+                    </h2>
+                )
+            })}
+        </>
+    )
+}
+
+export default People
